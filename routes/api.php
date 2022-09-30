92a000163a671f00c4c330ce61ad91a061f07c13
= <motasem.yakhni@gmail.com>
2022-09-30 20:05:37 +0300

    create sort string api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('sorted_string/{word}', [ApiController::class, 'sortString']);
