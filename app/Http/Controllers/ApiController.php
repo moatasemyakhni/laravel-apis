a5c82428f17e3940f5e001a5a24b770ea923ea40
= <motasem.yakhni@gmail.com>
2022-09-30 20:11:30 +0300

    ApiController<> create sortString method

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    function sortString(Request $req) {
+        $word = $req->word;
+        $nbArr = [];
+        $letters = str_split($word);
+
+        foreach($letters as $nb) {
+            if(!is_numeric($nb))
+                break;
+            else
+                $nbArr[] = array_shift($letters);
+        }
+
+        for($i=0;$i<sizeof($letters)-1;$i++) {
+            $j = $i + 1;
+            if(strtolower($letters[$i]) === $letters[$j]) {
+                $temp = $letters[$i];
+                $letters[$i] = $letters[$j];
+                $letters[$j] = $temp;
+            }
+        }
+
+        foreach($nbArr as $nb) {
+            $letters[] = $nb;
+        }
+
+        return response()->json([$word => implode($letters)]);
+    }
 }
