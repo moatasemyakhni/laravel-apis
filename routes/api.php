c7ae95daa115d0ebad167f468ef02758e5d16356
= <motasem.yakhni@gmail.com>
2022-09-30 20:25:57 +0300

    create decimal to binary number converter  api

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('sorted_string/{word}', [ApiController::class, 'sortString']);
 
 Route::get('break_number/{number}', [ApiController::class, 'breakNumber']);
+
+Route::get('number_to_binary/{sentence}', [ApiController::class, 'numberToBinary']);
